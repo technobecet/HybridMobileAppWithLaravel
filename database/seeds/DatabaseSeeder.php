3da09134a8bb53c7da3487c7aa4919f9714e8bf3
technobecet <technobecet@gmail.com>
2019-07-23 00:12:13 +0300

    Laravel Seeder - sample data on the project

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,12 +5,26 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(CitiesTableSeeder::class);
+        $this->call(ObjectsTableSeeder::class);
+        $this->call(AddressesTableSeeder::class);
+        $this->call(NotificationsTableSeeder::class);
+        $this->call(ArticlesTableSeeder::class);
+        $this->call(CommentsTableSeeder::class);
+        $this->call(LikeablesTableSeeder::class);
+        $this->call(PhotosTableSeeder::class);
+        $this->call(RoomsTableSeeder::class);
+        $this->call(ReservationsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(RoleUserTableSeeder::class);
+
     }
 }
+
